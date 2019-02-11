2d265d6cf3d247651f22dd20ae37fcd2011e6a9f
Jorge <jorgecampoy81@gmail.com>
2019-02-11 20:51:02 +0100

    Added Add button fav

--- src/app/pendientes/pendientes.page.ts
+++ src/app/pendientes/pendientes.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DeseosService } from '../servicios/deseos.service';
 import { Lista } from 'src/models';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component( {
   selector: 'app-pendientes',
@@ -11,10 +13,37 @@ export class PendientesPage implements OnInit {
 
 
 
-  constructor ( public deseosService: DeseosService ) { }
+  constructor (
+    public deseosService: DeseosService,
+    private alertController: AlertController,
+    private route: Router
+  ) { }
 
   listaSeleccionada( lista: Lista ) {
     console.log( lista );
+  }
+
+  async agregarLista() {
+    const alerta = await this.alertController.create( {
+      header: 'Nueva lista',
+      subHeader: 'Nombre de la nueva lista que se desea',
+      message: 'Nombre de la nueva lista',
+      buttons: [ {
+        text: 'Cancelar',
+      }, {
+        text: 'Agregar',
+        handler: data => {
+          if ( data.titulo === 0 ) { return; }
+          this.route.navigate( [ 'agregar/', data.titulo ] );
+          console.log( data );
+        }
+      } ],
+      inputs: [ {
+        name: 'titulo',
+        placeholder: 'Nombre de la lista'
+      } ]
+    } );
+    await alerta.present();
   }
 
   ngOnInit() {
